2cca79b600debdd394fe0e17a00b7b6d2c3a8f75
mellivorandy <anydoyi@gmail.com>
2025-03-01 12:41:06 +0800

    Add lru_cache module

---
+++ src/lru_cache.rs
@@ -0,0 +1,18 @@
+use std::collections::hash_map;
+use std::rc::{Rc, Weak};
+use std::cell::RefCell;
+
+struct Node {
+    tag: u32,
+    valid: bool,
+    prev: Weak<RefCell<Node>>,
+    next: Option<Rc<RefCell<Node>>>,
+}
+
+struct LRUCache {
+
+}
+
+impl LRUCache {
+
+}
