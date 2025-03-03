fb7baec016c91281e5e740547cb8d46c91ef325e
mellivorandy <anydoyi@gmail.com>
2025-03-03 08:48:13 +0800

    Update set.rs

--- src/set.rs
+++ src/set.rs
@@ -1,18 +1,49 @@
-use std::collections::hash_map;
+use std::collections::{hash_map, HashMap};
 use std::rc::{Rc, Weak};
 use std::cell::RefCell;
 
+enum NodeData {
+    Dummy,
+    Real { tag: u32, valid: bool },
+}
+
 struct Node {
-    tag: u32,
-    valid: bool,
+    data: NodeData,
     next: Option<Rc<RefCell<Node>>>,
     prev: Option<Weak<RefCell<Node>>>,
 }
 
 pub struct Set {
+    capacity: u32,
+    size: u32,
+    map: HashMap<u32, Rc<RefCell<Node>>>,
 
+    // dummy head, tail
+    head: Rc<RefCell<Node>>,
+    tail: Rc<RefCell<Node>>,
 }
 
 impl Set {
+    fn new(capacity: u32) -> Self {
+        let head = Rc::new(RefCell::new(Node {
+            data: NodeData::Dummy,
+            next: None,
+            prev: None,
+        }));
+        let tail = Rc::new(RefCell::new(Node {
+            data: NodeData::Dummy,
+            next: None,
+            prev: Some(Rc::downgrade(&head)),
+        }));
 
+        head.borrow_mut().next = Some(Rc::clone(&tail));
+
+        Set {
+            capacity,
+            size: 0,
+            map: HashMap::new(),
+            head,
+            tail,
+        }
+    }
 }
