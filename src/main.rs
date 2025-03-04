31687dc8c78c9c26da26e30eaff4944ac2438559
mellivorandy <anydoyi@gmail.com>
2025-03-04 19:46:16 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+use lru_cache::LRUCache;
+
 mod lru_cache;
 mod set;
 
@@ -28,13 +30,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .parse()
         .expect("Please provide a valid set degree (1/2/4/8)\n");
 
-    let block_size_byte = block_size << 2;
-    let cache_block_num = (cache_size << 10) / (block_size_byte as u32);
-    let set_num = cache_block_num / (set_degree as u32);
+    let mut lru_cache = LRUCache::new(cache_size, block_size, set_degree);
 
     // open file and process each line
-    let file = File::open(trace_file_name)
-        .expect("Failed to open the file\n");
+    let file = File::open(trace_file_name).expect("Failed to open the file\n");
     
     // use BufReader to improve the speed of reading trace file
     let reader = BufReader::new(file);
@@ -45,9 +44,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mem_addr = line.trim();
         let no_prefix_addr = mem_addr.trim_start_matches("0x");
         let addr_dec = u32::from_str_radix(&no_prefix_addr, 16).unwrap();
-        let mem_block_num = addr_dec / block_size_byte as u32;
-        let set_index = mem_block_num % set_num;
+        lru_cache.access(addr_dec);
     }
+
+    let miss_rate = lru_cache.count_miss_rate();
+    println!("Miss Rate = {:.6}", miss_rate);
 
     Ok(())
 }
