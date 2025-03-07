e63f494b01f3997b0ed33919f891f645acc4c92d
mellivorandy <anydoyi@gmail.com>
2025-03-07 18:15:29 +0800

    Update lru_cache.rs

--- src/lru_cache.rs
+++ src/lru_cache.rs
@@ -9,7 +9,7 @@ pub struct LRUCache {
 }
 
 impl LRUCache {
-    pub fn new(cache_size: u32, block_size: u32, set_degree: u32) -> Self {
+    pub fn new(cache_size: u32, block_size: u32, set_degree: usize) -> Self {
         let block_size_byte = block_size << 2;
         let cache_block_num = (cache_size << 10) / (block_size_byte as u32);
         let set_num = cache_block_num / (set_degree as u32);
@@ -27,11 +27,29 @@ impl LRUCache {
         }
     }
 
-    pub fn access(&mut self,  address: u32) {
-        
+    pub fn access(&mut self, address: u32) {
+        let memory_block = address / self.block_size_byte;
+        let set_index = memory_block % self.set_num;
+        let tag = memory_block / self.set_num;
+
+        let set = &mut self.sets[set_index as usize];
+
+        if set.get(tag) {
+            // hit
+            self.hit += 1;
+        } else {
+            // miss
+            self.miss += 1;
+            set.put(tag);
+        }
     }
 
     pub fn count_miss_rate(&self) -> f64 {
-        
+        let total = self.hit + self.miss;
+        if total == 0 {
+            0.0
+        } else {
+            self.miss as f64 / total as f64
+        }
     }
 }
