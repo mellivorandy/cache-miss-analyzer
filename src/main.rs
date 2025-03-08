54fa8d41ae6abec26a1eecb3c2bf50d294021bbc
mellivorandy <anydoyi@gmail.com>
2025-03-08 09:37:23 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+mod lru_cache;
+mod set;
+
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 use lru_cache::LRUCache;
-
-mod lru_cache;
-mod set;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
@@ -34,6 +34,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // open file
     let file = File::open(trace_file_name).expect("Failed to open file\n");
+    
     let reader = BufReader::new(file);
 
     for line_result in reader.lines() {
@@ -51,4 +52,48 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Miss Rate = {:.5}", miss_rate);
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+
+    #[test]
+    fn test_lru_with_trace_file() {
+        let file_path = "data/trace.txt";  
+        let file = File::open(file_path).expect("Failed to open file");
+        
+        let reader = BufReader::new(file);
+
+        // test args
+        let mut cache = LRUCache::new(1, 4, 2);
+
+        for line_result in reader.lines() {
+            let line = line_result.unwrap();
+            let mem_addr = line.trim();
+    
+            let no_prefix_addr = mem_addr.trim_start_matches("0x");
+            let addr_dec = u32::from_str_radix(&no_prefix_addr, 16).unwrap();
+            
+            cache.access(addr_dec);
+        }
+
+        println!(
+            "\nTest LRU with trace.txt => hits = {}, misses = {}, miss rate = {:.5}\n",
+            cache.hit, cache.miss, cache.count_miss_rate()
+        );
+
+        println!("set num = {}\n", cache.set_num);
+
+        for (i, set) in cache.sets.iter().enumerate() {
+            let list = set.debug_list();
+            
+            // only print non-empty sets, modify here to print all sets
+            if !list.is_empty() {
+                println!("Set {} => {:?}", i, list);
+            }
+        }
+    }
 }
